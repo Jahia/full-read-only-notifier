a14b33a9a2b9bcb16426982c6362c2a90406cec3
dsaulnier <dsaulnier@users.noreply.github.com>
2022-03-23 18:15:30 +0100

    Display the site settings panel only for the sites on which the module is enabled

--- src/main/resources/javascript/apps/register.js
+++ src/main/resources/javascript/apps/register.js
@@ -3,6 +3,7 @@ window.jahia.i18n.loadNamespaces('full-read-only-notifier');
 window.jahia.uiExtender.registry.add('adminRoute', 'fullReadOnlyNotifierManager', {
     targets: ['administration-sites:10'],
     requiredPermission: 'siteAdminUsers',
+    requireModuleInstalledOnSite: 'full-read-only-notifier',
     label: 'full-read-only-notifier:label',
     isSelectable: true,
     iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.fullReadOnlyNotifierManager.html'
